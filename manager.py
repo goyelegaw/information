ad7847681846a4483e19c452fd8a51c99b0d41ee
goyelegaw <fudu8449gxc@126.com>
2021-05-23 18:14:00 +0800

    设置入口函数

---
+++ manager.py
@@ -0,0 +1,21 @@
+from flask import session
+from flask_script import Manager
+from flask_migrate import Migrate, MigrateCommand
+
+from info import app, db
+
+
+manager = Manager(app)
+# 将 app 与 db 关联
+Migrate(app, db)
+# 将迁移命令添加到manager中
+manager.add_command('db', MigrateCommand)
+
+@app.route('/')
+def index():
+    session["name"] = "itheima"
+    return 'index'
+
+
+if __name__ == '__main__':
+    manager.run()
