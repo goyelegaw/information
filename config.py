abd62b314223d341e070971d35f3c33eae303f59
goyelegaw <fudu8449gxc@126.com>
2021-06-01 20:09:36 +0800

    设置日志

--- config.py
+++ config.py
@@ -1,3 +1,4 @@
+import logging
 from redis import StrictRedis
 
 
@@ -15,3 +16,29 @@ class Config():
     SESSION_USE_SIGNER = True
     SESSION_REDIS = StrictRedis(host=REDIS_HOST, port=REDIS_PORT)
     PERMANENT_SESSION_LIFETIME = 86400 * 2
+
+    LOG_LEVEL = logging.DEBUG
+
+class DevelopmentConfig(Config):
+    """开发环境下的配置"""
+    DEBUG = True
+    LOG_LEVEL = logging.WARNING
+
+
+
+class ProductionConfig(Config):
+    """生产环境下的配置"""
+    DEBUG = False
+
+
+class TestingConfig(Config):
+    """单元测试环境下的配置"""
+    DEBUG = True
+    TESTING = True
+
+
+config = {
+    "development": DevelopmentConfig,
+    "production": ProductionConfig,
+    "testing": TestingConfig
+}
--- manager.py
+++ manager.py
@@ -1,19 +1,26 @@
+import logging
 from flask import session
 from flask_script import Manager
 from flask_migrate import Migrate, MigrateCommand
 
-from info import app, db
+from info import create_app, db
 
-
+app = create_app("development")
 manager = Manager(app)
 # 将 app 与 db 关联
 Migrate(app, db)
 # 将迁移命令添加到manager中
 manager.add_command('db', MigrateCommand)
 
+
 @app.route('/')
 def index():
     session["name"] = "itheima"
+    logging.debug("This is a debug log.")
+    logging.info("This is a info log.")
+    logging.warning("This is a warning log.")
+    logging.error("This is a error log.")
+    logging.critical("This is a critical log.")
     return 'index'
 
 
